efce2a94fab919c62d88cc38a6356f71abf5b5fd
Abhinav Joshi <abhinavmjoshi@gmail.com>
2020-11-28 21:18:26 -0500

    initial intro page changes

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -14,31 +14,22 @@ class Intro extends React.Component {
         <div className="intro-content display-table">
           <div className="table-cell">
             <div className="container">
-              <h1 className="intro-title mb-4">Hello, I am XYZ</h1>
+              <h1 className="intro-title mb-4">Hi, I'm Abhinav Joshi</h1>
               <p className="intro-subtitle">
                 <span className="text-slider-items"></span>
                 <strong className="text-slider">
                   <Typed
                     strings={[
-                      "Front End Developer",
-                      "Back End Developer",
-                      "Software Engineer"
+                      "Student",
+                      "Innovator",
+                      "Full Stack Developer"
                     ]}
-                    typeSpeed={80}
+                    typeSpeed={70}
                     backDelay={1100}
                     backSpeed={30}
                     loop
                   />
                 </strong>
-              </p>
-              <p className="pt-3">
-                <a
-                  className="btn btn-primary btn js-scroll px-4"
-                  href="#work"
-                  role="button"
-                >
-                  View My Work
-                </a>
               </p>
             </div>
           </div>
