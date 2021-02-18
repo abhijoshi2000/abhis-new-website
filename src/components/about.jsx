f892729319c5fb00a9bc6cad9b86c2e6964e48ca
Abhinav Joshi <abhinavmjoshi@gmail.com>
2021-02-18 11:37:37 -0500

    Removed percentages from About Page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -12,35 +12,35 @@ class About extends React.Component {
           id: "JavaScript_skill",
           content: "JavaScript",
           porcentage: "75%",
-          value: "75"
+          value: "75",
         },
         { id: "NodeJS_skill", content: "Node", porcentage: "80%", value: "80" },
         {
           id: "ReactJS_skill",
           content: "ReactJS",
-          porcentage: "65%",
-          value: "65"
+          porcentage: "75%",
+          value: "75",
         },
         {
           id: "CPP_skill",
           content: "C++",
           porcentage: "55%",
-          value: "55"
+          value: "55",
         },
         {
           id: "MySQL_skill",
           content: "MySQL",
           porcentage: "65%",
-          value: "65"
+          value: "65",
         },
       ],
       about_me: [
         {
           id: "first-p-about",
           content:
-            "I am a current senior at Georgia Institute of Technology studying Computer Science. In the past, I have primarily dealt with full stack web development as well machine learning through a variety of internships and projects. I am interested in furthering my software development experience, along with relevant data science and machine learning capabilities, applying my interests to novel technologies and exploring unique sectors."
-        }
-      ]
+            "I am a current senior at Georgia Institute of Technology studying Computer Science. In the past, I have primarily dealt with full stack web development as well machine learning through a variety of internships and projects. I am interested in furthering my software development experience, along with relevant data science and machine learning capabilities, applying my interests to novel technologies and exploring unique sectors.",
+        },
+      ],
     };
   }
 
@@ -53,15 +53,15 @@ class About extends React.Component {
               <div className="box-shadow-full">
                 <div className="row">
                   <div className="title-box-2">
-                        <h5 className="title-left">About Me</h5>
-                      </div>
-                    {this.state.about_me.map(content => {
-                        return (
-                          <p className="lead" key={content.id}>
-                            {content.content}
-                          </p>
-                        );
-                      })}
+                    <h5 className="title-left">About Me</h5>
+                  </div>
+                  {this.state.about_me.map((content) => {
+                    return (
+                      <p className="lead" key={content.id}>
+                        {content.content}
+                      </p>
+                    );
+                  })}
                   <div className="col-md-6">
                     <div className="row">
                       <div
@@ -80,39 +80,35 @@ class About extends React.Component {
                       </div>
                     </div>
                     <div className="prof-pic-div">
-                      <img src={profPic}
-                           className="prof-pic"
-                           alt=""/> 
+                      <img src={profPic} className="prof-pic" alt="" />
                     </div>
                   </div>
                   <div className="col-md-6">
                     <div className="about-me pt-4 pt-md-0">
-                    <div className="title-box-2">
+                      <div className="title-box-2">
                         <h5> </h5>
                       </div>
                       <div className="skill-mf">
-                      {/* <p className="title-s">Skill</p> */}
-                      {this.state.skills.map(skill => {
-                        return (
-                          <React.Fragment key={skill.id}>
-                            <span>{skill.content}</span>{" "}
-                            <span className="pull-right">
-                              {skill.porcentage}
-                            </span>
-                            <div className="progress">
-                              <div
-                                className="progress-bar"
-                                role="progressbar"
-                                style={{ width: skill.porcentage }}
-                                aria-valuenow={skill.value}
-                                aria-valuemin="0"
-                                aria-valuemax="100"
-                              ></div>
-                            </div>
-                          </React.Fragment>
-                        );
-                      })}
-                    </div>
+                        {/* <p className="title-s">Skill</p> */}
+                        {this.state.skills.map((skill) => {
+                          return (
+                            <React.Fragment key={skill.id}>
+                              <span>{skill.content}</span>{" "}
+                              <span className="pull-right"></span>
+                              <div className="progress">
+                                <div
+                                  className="progress-bar"
+                                  role="progressbar"
+                                  style={{ width: skill.porcentage }}
+                                  aria-valuenow={skill.value}
+                                  aria-valuemin="0"
+                                  aria-valuemax="100"
+                                ></div>
+                              </div>
+                            </React.Fragment>
+                          );
+                        })}
+                      </div>
                     </div>
                   </div>
                 </div>
