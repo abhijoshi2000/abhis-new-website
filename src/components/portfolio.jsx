9affa1ef79c4d6e0ff1816d4e20edebda0e06fd9
Abhinav Joshi <abhinavmjoshi@gmail.com>
2021-03-21 16:58:02 -0400

    added new project, changed visuals

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-//import stock
-import flexify from "../img/flexify.png";
-import lebron from "../img/lebron.png";
+import flexify2 from "../img/flexify2.png";
+import allstars from "../img/allstars.jpeg";
 import pharma from "../img/pharma.jpg";
+import rwsbets from "../img/rwsbets.jpg";
 class Portfolio extends React.Component {
   render() {
     return (
@@ -26,7 +26,7 @@ class Portfolio extends React.Component {
               <div className="work-box">
                 <a href="https://github.com/abhijoshi2000/flexify">
                   <div className="work-img">
-                    <img src={flexify} alt="" className="img-fluid" />
+                    <img src={flexify2} alt="" className="img-fluid" />
                   </div>
                   <div className="work-content">
                     <div className="row">
@@ -34,7 +34,7 @@ class Portfolio extends React.Component {
                         <h2 className="w-title">flexify</h2>
                         <div className="w-more">
                           <span className="w-ctegory">
-                            ReactJS JavaScript Spotify Web API
+                            ReactJS | JavaScript | Spotify Web API
                           </span>
                         </div>
                       </div>
@@ -47,14 +47,14 @@ class Portfolio extends React.Component {
               <div className="work-box">
                 <a href="https://github.com/abhijoshi2000/nba-all-stars">
                   <div className="work-img">
-                    <img src={lebron} alt="" className="img-fluid" />
+                    <img src={allstars} alt="" className="img-fluid" />
                   </div>
                   <div className="work-content">
                     <div className="row">
                       <div className="col-sm-8">
                         <h2 className="w-title">nba-all-stars</h2>
                         <div className="w-more">
-                          <span className="w-ctegory">Python ML</span>
+                          <span className="w-ctegory">Python | Scikit-Learn</span>
                         </div>
                       </div>
                     </div>
@@ -73,7 +73,28 @@ class Portfolio extends React.Component {
                       <div className="col-sm-8">
                         <h2 className="w-title">pharma-latest</h2>
                         <div className="w-more">
-                          <span className="w-ctegory">Python Web Scraping</span>
+                          <span className="w-ctegory">Python | BeautifulSoup</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="work-box">
+                <a href="https://github.com/abhijoshi2000/r-wallstreebets-sentiment-analysis">
+                  <div className="work-img">
+                    <img src={rwsbets} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">r-wallstreebets-sentiment-analysis</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            Python | NLTK
+                          </span>
                         </div>
                       </div>
                     </div>
